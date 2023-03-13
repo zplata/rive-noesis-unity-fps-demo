3c7075dea342407fb167879593a432327fffa6a9
Zach Plata <plata.zach@gmail.com>
2023-03-13 17:23:54 -0500

    Add in boolean input for Ammo. Add Rive file for the objective complete (not hooked up yet)

--- Assets/NoesisGUI/MainHudViewModel.cs
+++ Assets/NoesisGUI/MainHudViewModel.cs
@@ -12,16 +12,21 @@ public class MainHudViewModel : MonoBehaviour, INotifyPropertyChanged
     private ActorsManager actorsManager;
     private Health playerhealth;
     private PlayerWeaponsManager weaponManager;
+    private PlayerInputHandler m_InputHandler;
+    private WeaponController activeWeaponController;
 
     private float health;
     private float ammo;
     private int enemiesLeft;
+    private bool isShooting;
 
     void Start()
     {
         GetComponent<NoesisView>().Content.DataContext = this;
         playerhealth = actorsManager.Player.GetComponent<Health>();
         EventManager.AddListener<EnemyKillEvent>(OnEnemyKilled);
+        m_InputHandler = GetComponent<PlayerInputHandler>();
+        activeWeaponController = weaponManager.GetActiveWeapon();
 
         EnemiesLeft = 2;
     }
@@ -29,8 +34,25 @@ public class MainHudViewModel : MonoBehaviour, INotifyPropertyChanged
     void Update()
     {
         Health = playerhealth.CurrentHealth;
-        var weaponController = weaponManager.GetActiveWeapon();
-        var ammoRatio = (int)Math.Round(weaponController.CurrentAmmoRatio * 100);
+        WeaponController activeThing = weaponManager.GetActiveWeapon();
+        if (activeThing != null && m_InputHandler != null)
+        {
+            /*            bool hasFired = activeThing.HandleShootInputs(
+                            m_InputHandler.GetFireInputDown(),
+                            m_InputHandler.GetFireInputHeld(),
+                            m_InputHandler.GetFireInputReleased());*/
+            bool hasFired = activeThing.CurrentAmmoRatio < 1.0f;
+            if (hasFired == true)
+            {
+                Debug.Log("HIT TRUE");
+                IsShooting = true;
+            }
+            else
+            {
+                IsShooting = false;
+            }
+        }
+        var ammoRatio = (int)Math.Round(activeThing.CurrentAmmoRatio * 100);
 
         if (ammoRatio >= 0)
         {
@@ -51,6 +73,7 @@ public class MainHudViewModel : MonoBehaviour, INotifyPropertyChanged
     {
         actorsManager = FindObjectOfType<ActorsManager>();
         weaponManager = FindObjectOfType<PlayerWeaponsManager>();
+        m_InputHandler = GetComponent<PlayerInputHandler>();
     }
 
     void OnDestroy()
@@ -94,7 +117,17 @@ public class MainHudViewModel : MonoBehaviour, INotifyPropertyChanged
         {
             if (this.enemiesLeft == value) return;
             this.enemiesLeft = value;
-            // Perhaps we can catch this event in XAML to then fire the Rive trigger?
+            OnPropertyChanged();
+        }
+    }
+
+    public bool IsShooting
+    {
+        get => this.isShooting;
+        set
+        {
+            if (this.isShooting == value) return;
+            this.isShooting = value;
             OnPropertyChanged();
         }
     }
